3010b6d6cb2beb36ee2fc5a2bab5bc9691661fc6
Rakshit7 <34086355+Rakshit7@users.noreply.github.com>
2023-12-12 16:24:16 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import UploadImg from "./components/UploadImg";
+
+function App() {
+  return (
+    <div className="App">
+      <UploadImg />
+    </div>
+  );
+}
+
+export default App;
